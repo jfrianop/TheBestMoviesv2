250d98fb1a901afeacb22b1b216a26bb87d83e85
jfrianop <jfrianop@gmail.com>
2019-10-02 19:01:21 -0500

    Add Change on Cards

--- js/app.js
+++ js/app.js
@@ -1,26 +1,54 @@
+//GET popular movies from API
 const fetchMovies = async () => {
     const response = await fetch('https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=3d7fd0461ae8d0f2e808c37fb41950d7')
     const jsonData = await response.json();
     return jsonData;
 }
 
-const displayMovies = (jsonData) => {
-    const imgURL = "https://image.tmdb.org/t/p/w300"
-    jsonData.results.forEach(movie => {
-        displayMovie(imgURL + movie.poster_path, movie.original_title, movie.overview);
+//GET details on specific Movie id
+const fetchMovie = async (id) => {
+    console.log("Fetching movie id " + id);
+    const response = await fetch('https://api.themoviedb.org/3/movie/' + id + '?language=en-US&api_key=3d7fd0461ae8d0f2e808c37fb41950d7')
+    const jsonData = await response.json();
+    return jsonData;
+}
+
+//Uses fetch movies to return an array with ids of popular movies
+const getPopularsId = async () => {
+    const response = await fetchMovies();
+    const idArr = response.results.map(movie => movie.id);
+    return idArr;
+}
+
+//Receives id[] and prints every movie of each id
+const displayMovies = (idArr) => {
+    let test = [1, 2, 3, 4];
+    console.log("Arrays to Display");
+    console.log(idArr);
+    console.log(test);
+    const imgURL = "https://image.tmdb.org/t/p/w200";
+    console.log("Item del arreglo: " + idArr[0])
+
+    idArr.forEach(id => {
+        console.log(id + 2)
+        fetchMovie(id).then(movie => {
+            displayMovie(imgURL + movie.poster_path, movie.original_title, movie.overview, movie.vote_average,
+                movie.runtime, movie.relase_date, movie.genres[0].name, movie.id);
+        })
     });
 }
 
-const displayMovie = (img, title, description) => {
-    $(".card-container").append('<div class="card m-3" style = "max-width: 18rem;" > <img src="' +
-        img + '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
-        title + '</h5><p class="card-text">' + description + '</p></div></div>')
+
+const displayMovie = (imgCode, title, description, score, duration, date, genre, id) => {
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var context = { imgCode, title, description, score, duration, date, genre, id };
+    var card = template(context);
+    $(".card-container").append(card);
 }
 
-const importMovies = () => {
-    $("#import").hide();
-    fetchMovies().then((e => {
-        displayMovies(e);
-    }))
+const importMovies = async () => {
+    let moviesIds = await getPopularsId();
+    displayMovies(moviesIds);
 }
-$("#import").on("click", importMovies);
+$(document).ready(importMovies);
